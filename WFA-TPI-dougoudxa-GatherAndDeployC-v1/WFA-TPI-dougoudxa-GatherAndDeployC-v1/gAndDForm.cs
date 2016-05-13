494557b6c19e30bfab8f78508c13e007f13c838e
Exelios <exelios.alendiel@gmail.com>
2016-05-13 11:23:25 +0200

    Backup - 13.05.2016 - 11:23

--- WFA-TPI-dougoudxa-GatherAndDeployC-v1/WFA-TPI-dougoudxa-GatherAndDeployC-v1/gAndDForm.cs
+++ WFA-TPI-dougoudxa-GatherAndDeployC-v1/WFA-TPI-dougoudxa-GatherAndDeployC-v1/gAndDForm.cs
@@ -15,6 +15,18 @@ namespace WFA_TPI_dougoudxa_GatherAndDeployC_v1
         public appForm()
         {
             InitializeComponent();
+
+            //Testing purposes.
+            int dummyEntries = 17;
+            TargetHost tempHost;
+
+            for (int index = 0; index < dummyEntries; ++index)
+            {
+                tempHost = new TargetHost("\\\\INF-N511-" + (index + 1).ToString("00"), "dummy test", index);
+
+                hostPanelContainer.Controls.Add(tempHost.getTargetHostPanel());
+            }
+            //End of test
         }
     }
 }
